bf981009f09a829db1393e1fd6b4fb2ee16c9632
Mehmet Yiğit Çakır <cakirmehmetyigit@gmail.com>
2026-01-28 16:03:12 +0300

    Creating array

--- a.java
+++ a.java
@@ -1,4 +1,18 @@
+import java.util.Random;
+
 public class a {
+
+ public static int[] CreateArray(int size){
+  Random rndm = new Random();   
+   int[] newArr = new int[size];
+
+  for (int i = 0; i < size; i++ ){
+     newArr[i] = rndm.nextInt(100);
+  }
+ return newArr;
+
+
+ }
 
     
 }
