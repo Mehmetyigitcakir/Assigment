acac8cc9089cecc4c19d5eb3a1e1853cc0e983bd
mehmeteyupornek1 <mehmeteyupornek1@gmail.com>
2026-01-28 15:48:43 +0300

    Maxmin

--- c.java
+++ c.java
@@ -1,3 +1,20 @@
 public class c {
-    
+    public static int maxvalue=Integer.MIN_VALUE;
+    public static int minvalue=Integer.MAX_VALUE;
+    public static int Maxfinder(int[] array){
+    for(int i=0;i<array.length;i++){
+        if(array[i]>maxvalue){
+            maxvalue=array[i];
+        }
+    }
+    return maxvalue;
+    }
+    public static int Minfinder(int[] array){
+        for(int i=0;i<array.length;i++){
+            if(array[i]<maxvalue){
+                minvalue=array[i];
+            }
+        }
+        return minvalue;
+    }
 }
