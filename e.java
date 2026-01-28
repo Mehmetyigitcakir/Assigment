938e9350590571ab7cc82e20dc85637243fb0c42
Arda Ulu <arda.ulu@ug.bilkent.edu.tr>
2026-01-28 15:51:50 +0300

    Sum Odd Even

--- e.java
+++ e.java
@@ -1,3 +1,19 @@
 public class e {
-    
+// hello
+    public static int findOddSum(int arr[]){
+        int oddSum = 0;
+        for (int i = 0; i < arr.length; i++){
+            if (i % 2 == 1){
+                oddSum += arr[i];
+            }
+        }
+    }
+    public static int findEvenSum(int arr[]){
+        int evenSum = 0;
+        for (int i = 0; i < arr.length; i++){
+            if (i % 2 == 0){
+                evenSum += arr[i];
+            }
+        }
+    }
 }
