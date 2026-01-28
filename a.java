e15ca7d8aea3b1f652415daab4d15cfca42a2a0e
Mehmet Yiğit Çakır <cakirmehmetyigit@gmail.com>
2026-01-28 17:32:43 +0300

    Getting array size from the user.

--- a.java
+++ a.java
@@ -1,18 +1,26 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class a {
 
- public static int[] CreateArray(int size){
+ public static int[] CreateArray(){
+  int size;
+
+  Scanner scan = new Scanner(System.in);
+  System.out.print("Please Enter an array size: ");
+  size = scan.nextInt();
+
   Random rndm = new Random();   
    int[] newArr = new int[size];
 
   for (int i = 0; i < size; i++ ){
      newArr[i] = rndm.nextInt(100);
-  }
+
+   }
+
+ scan.close();
  return newArr;
 
-
  }
-
     
 }
